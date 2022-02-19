b79d2d670c4a3824e74f8d22328a1d74ca2cbc7f
Harutyunyan1997 <82769438+Harutyunyan1997@users.noreply.github.com>
2022-02-19 14:29:19 +0400

    Add files via upload

---
+++ src/styles/backpack.jsx
@@ -0,0 +1,13 @@
+
+const backpack = () => {
+    return (
+        <div className="back-wrapper">
+            <ul className="back-list">
+                <li className="newitem"></li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default backpack;
